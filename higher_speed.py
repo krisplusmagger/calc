8e6fd2560ceb75dc2d80e0e26c2aeff3e20020ac
Mac <mac@MacdeMacBook-Air.local>
2023-06-16 13:02:29 +0800

    add two threads

--- higher_speed.py
+++ higher_speed.py
@@ -8,6 +8,12 @@ from plot import Plot
 from findcharacter import Find_Character
 from demfgenerator import DtmfGenerator
 
+import pyaudio
+import wave
+import threading
+import time
+from scipy.io import wavfile
+
 from pycalc import PyCalcWindow, PyCalc
 from unittest import result#导入sys. 该模块提供了exit()用于彻底终止应用程序的功能
 ##import commpy.channels.awgn as awgn
@@ -76,17 +82,81 @@ def bit_error(original_list, recognition_output):
             error_bits += 1
 
     return float(error_bits / len(original_list))
+
+def record_audio(filename, duration):
+    CHUNK = 1024  # 每次读取的音频数据块大小
+    FORMAT = pyaudio.paInt16  # 音频数据的格式
+    CHANNELS = 1  # 声道数
+    RATE = 8000  # 采样率
+    RECORD_SECONDS = duration  # 录制的时长
+
+    p = pyaudio.PyAudio()  # 实例化PyAudio对象
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    frames = []  # 用于存储录制的音频数据
+
+    print("开始录制音频...")
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK) #录制音频，并将数据送入到data中
+        frames.append(data)
+
+    print("录制完成.")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    # 将录制的音频数据写入WAV文件
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+    return frames
+
+def play_audio(filename):
+    print("开始播放音频...")
+    playsound(filename)
+    print("播放完成.")
     
+
+
+
 def main():
-    
-    input_list = generate_random_input(12) #生成128位伪随机序列
-    dtmf_sig = dtmf(input_list,snr = 6) #生成dtmf信号,信噪比为0dB
+
+    input_length = 128
+    input_list = generate_random_input(input_length) #生成128位伪随机序列
+    dtmf_sig = dtmf(input_list,snr = 0) #生成dtmf信号,信噪比为6dB
     results =  recognition(sam_freq=8000,input_signal = dtmf_sig) #识别信号，并将识别结果存储在results变量中
     error_rates = bit_error(input_list,results)
     print(error_rates)
-    name= 'haha'+'.wav'
-    playsound(name)
+    # # 设置录制参数
+    # filename = 'record.wav'
+    # name = 'haha.wav'
+    # duration = input_length * 0.1  # 录制时长为5秒
 
+    # # 创建线程进行音频录制和播放
+    # record_thread = threading.Thread(target=record_audio, args=(filename, duration))#录制的文件
+    # play_thread = threading.Thread(target=play_audio, args=(name,))
+
+    # # 启动线程
+    # record_thread.start()
+    # play_thread.start()
+
+    # # 等待录制线程和播放线程结束
+    # record_thread.join()
+    # play_thread.join()
+    # #name= 'haha'+'.wav'
+    # #playsound(name)
+    # sample_rate, audio_data = wavfile.read(filename)
+    # print(sample_rate, audio_data)
     
 if __name__ == "__main__":
     main()        
